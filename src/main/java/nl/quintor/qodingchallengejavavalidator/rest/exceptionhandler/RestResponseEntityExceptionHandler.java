8cae3b94b57c9f269bc3a00278adbe6783081307
BeingBright <BrightonvandenEnd@gmail.com>
2019-12-16 12:52:22 +0100

    Update RestResponseEntityExceptionHandler.java

--- src/main/java/nl/quintor/qodingchallengejavavalidator/rest/exceptionhandler/RestResponseEntityExceptionHandler.java
+++ src/main/java/nl/quintor/qodingchallengejavavalidator/rest/exceptionhandler/RestResponseEntityExceptionHandler.java
@@ -5,7 +5,6 @@ import nl.quintor.qodingchallengejavavalidator.rest.customexception.JSONCustomEx
 import nl.quintor.qodingchallengejavavalidator.service.exception.CanNotCompileException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,44 +12,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.sql.SQLException;
-
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     private final Logger logger = LoggerFactory.getLogger(RestResponseEntityExceptionHandler.class);
-
-    @ExceptionHandler({SQLException.class})
-    public ResponseEntity<Object> handleNotFoundStatus(Exception e, WebRequest request) {
-        JSONCustomExceptionSchema exceptionResponse =
-                new JSONCustomExceptionSchema(
-                        e.getMessage()
-                );
-        logger.error(e.fillInStackTrace().toString());
-        return new ResponseEntity<>(exceptionResponse, new HttpHeaders(), HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler({
-
-    })
-    public final ResponseEntity<Object> handleCustomExceptionInternalServerError(CustomException ex, WebRequest webRequest) {
-        JSONCustomExceptionSchema exceptionResponse =
-                new JSONCustomExceptionSchema(
-                        ex.getMessage(), ex.getDetails(), ex.getNextActions(), ex.getSupport()
-                );
-        logger.error(ex.getMessage(), ex.getDetails(), ex.fillInStackTrace().toString());
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
+    
     @ExceptionHandler({
             CanNotCompileException.class
     })
-    public final ResponseEntity<Object> handleCustomExceptionNotFound(CustomException ex, WebRequest webRequest) {
+    public final ResponseEntity<Object> handleCustomExceptionExpectationFailed(CustomException ex, WebRequest webRequest) {
         JSONCustomExceptionSchema exceptionResponse =
                 new JSONCustomExceptionSchema(
                         ex.getMessage(), ex.getDetails(), ex.getNextActions(), ex.getSupport()
                 );
         logger.error(ex.getMessage(), ex.getDetails(), ex.fillInStackTrace().toString());
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.EXPECTATION_FAILED);
     }
 }
